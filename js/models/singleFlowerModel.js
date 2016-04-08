09aa39712e042362408c115710f32294c5b17756
Dan Sajjad <dan.sajjad@gmail.com>
2016-04-08 19:40:29 -0400

    Watch for changes to a model using initialize(), get(), set(), on()

--- js/models/singleFlowerModel.js
+++ js/models/singleFlowerModel.js
@@ -7,6 +7,24 @@ app.singleFlower = Backbone.Model.extend({ //namespacing because some other vari
   defaults: { //default properties
     color: "pink",
     img: "images/placeholder.jpg"
-  }
+  },
+
+  //code below is not necessary but we will look at how to watch for instance changes
+  initialize: function() {//will run everytime a new instance is created
+    console.log("A model instance named " + this.get("name") +
+    " has been created and it costs " + this.get("price"));
+
+    //now to add the "on" method which watches for changes;
+    this.on("change", function() {//looks for all changes
+      console.log("Something in our model has changed.");
+      //to check how this works got to flowerApp.js and
+      //using .set() change an instance
+    });
+
+    this.on("change:price", function() {//looks for changes in price
+      console.log("The price for the " + this.get("name") +
+      " model just changed to $" + this.get("price") + " dollars.")
+    });
+  }//initialize
 
 });
